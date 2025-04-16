5a8e62dd242383944412b6a0ab9f3eb03a77d07e
Bruno Capuano <bcapuano@gmail.com>
2025-04-16 10:54:11 -0400

    Update MemoryContext and package references
    DETAILS
    
    - Modified `MemoryContext.cs` to use `VectorSearchOptions<ProductVector>` for improved type safety.
    - Updated package references in `Products.csproj` to newer versions for `Aspire.Azure.AI.OpenAI`, `Aspire.Microsoft.EntityFrameworkCore.SqlServer`, and `Microsoft.SemanticKernel.Connectors.InMemory`.
    - Upgraded `OpenTelemetry.Instrumentation.AspNetCore` in `Store.csproj` from version 1.11.0 to 1.11.1.
    - Changed multiple `Aspire.Hosting` package references in `eShopAppHost.csproj` from version 9.1.0 to 9.2.0.
    - Updated several packages in `eShopServiceDefaults.csproj`, including `Azure.Monitor.OpenTelemetry.AspNetCore` and various `OpenTelemetry` packages, to their latest versions.

--- src/02 Apire Azure Samples/Products/Memory/MemoryContext.cs
+++ src/02 Apire Azure Samples/Products/Memory/MemoryContext.cs
@@ -97,10 +97,9 @@ public class MemoryContext
             var result = await _embeddingClient.GenerateEmbeddingAsync(search);
             var vectorSearchQuery = result.Value.ToFloats();
 
-            var searchOptions = new VectorSearchOptions()
+            var searchOptions = new VectorSearchOptions<ProductVector>()
             {
-                Top = 1,
-                VectorPropertyName = "Vector"
+                Top = 1
             };
 
             // search the vector database for the most similar product        
